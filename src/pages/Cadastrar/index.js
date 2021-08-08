c729a7a3be0dc767af237d12475456ce4fc7a946
lfdinisfernandes <lfdinisfernandes@gmail.com>
2021-08-08 17:26:50 -0300

    criando a parte de cadastrar no front

--- src/pages/Cadastrar/index.js
+++ src/pages/Cadastrar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import {Menu} from '../../componentes/Menu';
 
 export const Cadastrar = () => {
@@ -9,19 +9,26 @@ export const Cadastrar = () => {
         Quantidade: ''
     })
 
+    const valueInput = e => setProduto({...produto, [e.target.name]: e.target.value});
+
+    const addProduto = async e =>{
+        e.preventDefault();
+        console.log("Nome: " + produto.nome);
+    }
+
     return(
         <>
         <Menu />
             <h1>Cadastrar</h1>
-            <form>
-            <label>Nome: </label>
-                <input type="text" name="nome" placeholder="Nome do produto" /><br /><br />
+            <form ondsubmit={addProduto}>
+                <label>Nome: </label>
+                <input type="text" name="nome" placeholder="Nome do produto" onchange={valueInput} /><br /><br />
 
                 <label>Valor: </label>
-                <input type="text" name="Valor" placeholder="Preço do produto" /><br /><br />
+                <input type="text" name="Valor" placeholder="Preço do produto"onchange={valueInput} /><br /><br />
 
                 <label>Quantidade: </label>
-                <input type="number" name="Quantidade" placeholder="Quantidade do produto" /><br /><br />
+                <input type="number" name="Quantidade" placeholder="Quantidade do produto"onchange={valueInput} /><br /><br />
 
                 <button type="submit">Cadastrar</button>
             </form>
